d8885cb670f2850acee67a8bc50e47592b5f15d5
Yvan Deniro <azanfacknkenhoungyvandeniro@gmail.com>
2022-11-14 11:45:18 +0100

    ajouter un etudiant

--- Headers/fonctions.h
+++ Headers/fonctions.h
@@ -1,16 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef FONCTIONS_H_INCLUDED
 #define FONCTIONS_H_INCLUDED
 
-int code;
+int code = 0;
 char nom[20], prenom[20];
-float noteE, noteO;
+float noteE;
 FILE * fp;
 
 void ajouterETD(void);
-void bulletin(void);
-void modifier(void);
+void recherchETD(void);
+void modifierETD(void);
 void supprimerETD(void);
-void moyenneClase(void);
-void listeAdmis(void);
+
+    void ajouterETD(){
+    int codeR = 0, tr=0;  
+    fp = fopen("note.txt", "at+");
+    if(fp==NULL){
+        printf("Impossible d'ouvrire le fichier \n");
+        exit(1);
+    }
+    printf("Donnez le code : "); scanf("%d",&codeR);
+
+    while(!feof(fp)){
+        fscanf(fp, "%d\n%s\n%s\n%f\n",&code,nom,prenom,&noteE);
+        if (code == codeR){ tr = 1; break; }
+    }   
+
+    if (tr==1) printf("etudiant exist deja!! \n");
+    else{
+        printf("NOM : "); scanf("%s",nom);
+        printf("PRENOM : "); scanf("%s",prenom);
+        do{ printf("NOTE : "); scanf("%f",&noteE); } while(noteE<0 || noteE>20);
+        fprintf(fp, "%d\n%s\n%s\n%f\n",codeR,nom,prenom,noteE);
+        }
+    fclose(fp);
+    }
+
+    void recherchETD(){
+        int codeR, tr = 0;
+        fp = fopen("note.txt","r");
+        if(fp==NULL){
+            printf("Impossible d'ouvrire le fichier \n");
+            exit(1);
+        }
+        printf("Donnez le code : "); scanf("%d",&codeR);
+        while (!feof(fp))
+        {
+            fscanf(fp, "%d\n%s\n%s\n%f\n",&code,nom,prenom,&noteE);
+
+            if (code == codeR){
+                tr = 1;
+                printf("CODE\tNOM\tPRENOM\tNOTE\n");
+                printf("%d\t%s\t%s\t%.2f",code, nom, prenom, noteE);
+            }
+        }
+            if (tr==0){
+                printf("L'etudiant n'existe pas!!\n");
+            }
+        fclose(fp);   
+    }
+
+
 
 #endif  // FONCTIONS_H_INCLUDED
--- Sources/main.c
+++ Sources/main.c
@@ -15,8 +15,6 @@ int main(){
         printf("2-Afficher bulletin\n");
         printf("3-Modifier le nom\n");
         printf("4-Supprimer un Etudiant\n");
-        printf("5-Afficher la moyenne de la classe\n");
-        printf("6-Liste des admis\n");
         printf("0-Fin de traitement\n");
         printf("votre choix !");
         scanf("%d", &choix);
@@ -24,9 +22,15 @@ int main(){
         switch (choix){
             case 1: ajouterETD(); break;
 
+            case 2: recherchETD(); break;
+
+            case 3: modifierETD(); break; 
+
+            case 4: supprimerETD(); break;
+
             case 0: printf("fin de traitement!"); break;
     
-            default: printf("Choix errone! choix entre [1-6]\n\n"); break;
+            default: printf("Choix errone! choix entre [1-4]\n\n"); break;
         }
         getch();
 
